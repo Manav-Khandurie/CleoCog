083c64468b5ef0208bf90c69ce60b6593f041295
Manav Khandurie <manavkhandurie@gmail.com>
2025-05-13 15:10:48 +0530

    WIP k8s Migration

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsConfigPaths from 'vite-tsconfig-paths';
+import tsConfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
-  base: "./"
+  base: "./",
+  server: {
+    allowedHosts: ['*'],
+    host: true, // Allow access from any external host (IP, ngrok, etc.)
+    port: 5173  // Optional: specify port
+  },
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  }
 })
